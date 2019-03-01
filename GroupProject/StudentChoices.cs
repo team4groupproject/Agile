094bc6d01173b124fb336b5ee7364965a72f4053
Marco Olivera <timetrek@aol.com>
2019-03-01 01:10:53 -0800

    Marco Olivera
    The correct solution to my previous upload

--- GroupProject/StudentChoices.cs
+++ GroupProject/StudentChoices.cs
@@ -1,20 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
 
 namespace GroupProject
 {
-    public partial class Student : Form
+    public partial class StudentChoices : Form
     {
-        public Student()
+        string connectionString;
+        SqlConnection conn;
+        BindingSource studentBindingSource = new BindingSource();
+        int studentID;
+
+        public StudentChoices()
         {
             InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["GroupProject.Properties.Settings.TinyCollegeDBConnectionString"].ConnectionString;
+        }
+
+        private void btnFindStudentID_Click_1(object sender, EventArgs e)
+        {
+            using (conn = new SqlConnection(connectionString))
+            using (SqlCommand comd = new SqlCommand("SELECT * FROM student WHERE studentID = @studentID", conn))
+            using (SqlDataAdapter adapterStudent = new SqlDataAdapter(comd))
+            {
+                //use the studentID which user entered in text box to pull the data in SQLScript
+                comd.Parameters.AddWithValue("@studentID", txtStudentID.Text);
+                //create a DataTable in working memory for retrieved data
+                DataTable studentTable = new DataTable();
+                //use SQLscript to retrieve and fill data table
+                adapterStudent.Fill(studentTable);
+                //check to see if any data was pulled based on ID entered
+                if (studentTable.Rows.Count < 1)
+                { MessageBox.Show("Not a valid Student ID"); }//show feedback if ID entered does not exist in DB
+                else
+                {
+                    //name first row of table in working memory (this is the only row since studentID is unique)
+                    DataRow rowStudent = studentTable.Rows[0];
+                    studentID = int.Parse(rowStudent["studentID"].ToString()); //define shared public variable
+                    //load student data based on row found 
+                    txtFirstNameStudent.Text = rowStudent["studentFirstName"].ToString();
+                    txtLastNameStudent.Text = rowStudent["studentLastName"].ToString();
+
+                    //load DataGrid
+                    dgvCourses.DataSource = studentBindingSource;
+                    //select the data needed for DataGrid and to calculate credits completed                
+                    //************ Add grades from registration table*********************
+                    using (SqlCommand command =new SqlCommand ("SELECT course.courseName, course.courseCreditHours, registration.grade AS Grade FROM registration " +
+                        "INNER JOIN session ON registration.sessionId = session.sessionId " +
+                        "INNER JOIN course ON session.courseId = course.courseId " +
+                        "WHERE registration.studentID=@studentID", conn))
+                    using (SqlDataAdapter adapterCourses = new SqlDataAdapter(command))
+                    {
+                        command.Parameters.AddWithValue("@studentID", studentID);
+                        SqlCommandBuilder comdBuilder = new SqlCommandBuilder(adapterCourses);//?
+                        //DataTable in working memory to contain query results
+                        DataTable registrationTable = new DataTable();
+                        adapterCourses.Fill(registrationTable);
+                        studentBindingSource.DataSource = registrationTable;
+
+                        //******adjust to only count completed courses with a grade***************
+                        //sum credit hours 
+                        int creditsEarned= 0; //clear out any previous search results
+                        //do for each row in registration table
+                        for (int x =0; x < registrationTable.Rows.Count; x++)
+                        {
+                            DataRow currentRow = registrationTable.Rows[x];
+                            //char grade = char.Parse(currentRow["grade"].ToString());
+                            //if(grade == 'A' || grade == 'B' || grade == 'C')
+                            //{
+                                creditsEarned = creditsEarned + int.Parse(currentRow["courseCreditHours"].ToString());
+                            //}
+                        }
+                        txtEarnedCredits.Text = creditsEarned.ToString();
+                        if (creditsEarned>120)
+                        { MessageBox.Show("Congratulations, you have graduated from Tiny College"); }//show feedback if student has graduated
+                    }
+
+                    //enable register button if student found
+                    btnRegister.Enabled = true;
+                }
+            }
+
+        }
+
+        private void StudentChoices_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'tinyCollegeDB1DataSet.registration' table. You can move, or remove it, as needed.
+            //this.registrationTableAdapter.Fill(this.tinyCollegeDBDataSet.registration);
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            //create a new form for registration and pass the studentID entered to new form
+            StudentRegister newRegisterForm = new StudentRegister(studentID);
+            newRegisterForm.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
---
+++ GroupProject/StudentRegister.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace GroupProject
+{
+    public partial class StudentRegister : Form
+    {
+        string connectionString;
+        SqlConnection conn;
+        int studentID;
+        int sessionID;
+        int seatsFilled;
+        int remainingSeats;
+
+        //constructor accepts StudentID from previous form
+        public StudentRegister(int stuID)
+        {
+            //share the studentID to public variable passed to all the methods
+            studentID = stuID;
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["GroupProject.Properties.Settings.TinyCollegeDBConnectionString"].ConnectionString;
+        }
+
+        private void StudentRegister_Load(object sender, EventArgs e)
+        {
+            //display student id for student who will be registered (value was passed from previous form)
+            txtStudentID.Text = studentID.ToString();
+
+            using (conn = new SqlConnection(connectionString))
+            using (SqlCommand comd = new SqlCommand("SELECT courseName, courseId FROM course", conn))
+            using (SqlDataAdapter adapterCourse = new SqlDataAdapter(comd))
+            {
+                //create a DataTable in working memory for retrieved data
+                DataTable CourseTable = new DataTable();
+                //use SQLscript to retrieve and fill data table
+                adapterCourse.Fill(CourseTable);
+                //show course name in ComboBox
+                cboCourse.DisplayMember = "courseName";
+                //use id behind selection in ComboBox
+                cboCourse.ValueMember = "courseId";
+                cboCourse.DataSource = CourseTable;
+            }
+        }
+
+        private void cboCourse_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (conn = new SqlConnection(connectionString))
+            //show only sessions for selected course. (retrieve max seats of course for calculating remainingSeats)
+            using (SqlCommand comd = new SqlCommand("SELECT session.courseId, session.sessionId, session.sessionSeatsFilled, course.courseMaxNumSeats " +
+                "FROM session " +
+                "INNER JOIN course ON session.courseId=course.courseId " +
+                "WHERE session.courseId = @CourseID", conn))
+            using (SqlDataAdapter adapterSession = new SqlDataAdapter(comd))
+            {
+                //Set courseID in comd to be the value of selected in course ComboBox
+                comd.Parameters.AddWithValue("@CourseID", cboCourse.SelectedValue);
+
+                //create a DataTable in working memory for retrieved data
+                DataTable SessionTable = new DataTable();
+                //use SQLscript to retrieve and fill data table
+                adapterSession.Fill(SessionTable);
+                //show sessionID in ComboBox
+                cboSession.DisplayMember = "sessionId";
+                //use id for value
+                cboSession.ValueMember = "sessionId";
+                cboSession.DataSource = SessionTable;
+
+                //calculate and show remaining seats
+                DataRow dr = SessionTable.Rows[0];
+                int maxSeats = int.Parse(dr["courseMaxNumSeats"].ToString());
+                seatsFilled = int.Parse(dr["sessionSeatsFilled"].ToString());
+                remainingSeats = maxSeats-seatsFilled;
+                txtSeatsRemaining.Text = remainingSeats.ToString();
+                //set public variable session ID (will be updating that table in registration method)
+                sessionID = int.Parse(dr["sessionId"].ToString());
+
+                //make button available only if seats remain in this session
+                if (remainingSeats > 0)
+                {
+                    btnRegister.Enabled = true;
+                }
+                else
+                { btnRegister.Enabled = false; }
+            }
+        }
+
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            using (conn = new SqlConnection(connectionString))
+            using (SqlCommand comd = new SqlCommand("INSERT INTO registration (sessionId, studentId) " +
+                "VALUES (@sessionId, @studentId)", conn))
+            {
+                conn.Open();
+                comd.Parameters.AddWithValue("@sessionId", cboSession.Text);
+                comd.Parameters.AddWithValue("@studentId", studentID);
+                comd.ExecuteScalar();
+                conn.Close();
+            }
+
+            //update seats filled to one higher
+            using (conn = new SqlConnection(connectionString))
+            using (SqlCommand comdSeats = new SqlCommand("UPDATE session SET sessionSeatsFilled = @seatsFilled WHERE sessionId = @sessionId", conn))
+            {
+              conn.Open();
+            //add 1 to public variable seatsFilled- set in course cbo index change method
+            comdSeats.Parameters.AddWithValue("@seatsFilled", seatsFilled + 1);
+            comdSeats.Parameters.AddWithValue("@sessionId", sessionID);
+            comdSeats.ExecuteScalar();
+            }
+            MessageBox.Show("Registered for session");
+
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
